f5d33912c4f77455221fdb52a054e4453f2e1bdf
Jitesh Singh Shekhawat <jitesh@Jiteshs-MacBook-Air.local>
2025-06-25 22:51:20 +0530

    Added functions to route

--- apps/backend/src/routes/index.ts
+++ apps/backend/src/routes/index.ts
@@ -1,16 +1,53 @@
+import { UrlModel } from "@repo/db";
 import { Router ,Request , Response} from "express";
 
 export const router = Router(); 
 
 
-router.get('/:shortUrl',(req: Request,res: Response)=>{
-    const shortUrl = req.params; 
-    res.send('Works Fineee !!!!'); 
+function UrlGenerator(){
+    // I have to Implement it ::::
+
+    return "*****"; 
+}
+
+router.get('/:shortUrl',async (req: Request,res: Response)=>{
+    const {shortUrl} = req.params; 
+
+    try {
+        const found = await UrlModel.findOne({ shortUrl });
+    
+        if (found && found.longUrl) {
+          return res.redirect(found.longUrl);
+        } else {
+          return res.status(404).json({ message: "Short URL not found" });
+        }
+      } catch (error) {
+        return res.status(500).json({ message: "Server error", error });
+      }
 }); 
 
+router.post('/create-url',async (req:Request, res: Response)=>{
 
-router.post('/create-url',(req:Request, res: Response)=>{
     const longUrl = req.body.longUrl; 
+    if(!longUrl){
+        return res.status(400).json({message: "No Long Url Found"}); 
+    }
 
-    
+    try{
+        const existing = await UrlModel.findOne({longUrl}); 
+
+        if(existing){
+            return res.json(existing); 
+        }
+
+        const shortUrl = UrlGenerator();  
+
+        const newEntry = new UrlModel({longUrl,shortUrl}); 
+
+        await newEntry.save(); 
+
+        return res.status(201).json(newEntry); 
+    }catch(Error){
+        return res.status(500).json({message: "Server Error",Error}); 
+    }
 }); 
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import { UrlModel } from './models/url.model';
 
+
 const connectToDatabase = async () => {
   const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/urlshortner';
 
