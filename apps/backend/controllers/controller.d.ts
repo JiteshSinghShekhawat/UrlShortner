4cd610389308776327ef41d68362c9ff3a829d99
Jitesh Singh Shekhawat <jitesh@Jiteshs-MacBook-Air.local>
2025-07-02 11:38:07 +0530

    Deleted Extra Folder

--- apps/backend/controllers/controller.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response } from "express";
-export declare const shortUrl: (req: Request, res: Response) => Promise<void | Response<any, Record<string, any>>>;
-export declare const createUrl: (req: Request, res: Response) => Promise<Response<any, Record<string, any>>>;
--- apps/backend/controllers/generation.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const generateString: (n: number) => string;
-export declare const createShortUrl: (n: number) => string;
-export declare const urlToNumber: (uri: string) => number;
